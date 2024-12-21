557a345e5f2aae6b2e95181ac070425f7fabf28b
symball <contact@simonball.me>
2024-12-21 12:27:12 +0000

    update packages through ncu and change default controller message to return hello world

--- src/features/default/controllers/default.controller.ts
+++ src/features/default/controllers/default.controller.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express'
 class DefaultController {
     public index = (req: Request, res: Response, next: NextFunction): void => {
         try {
-            res.status(200).json({ message: 'OK' })
+            res.status(200).json({ message: 'hello world' })
         } catch (error) {
             next(error)
         }
